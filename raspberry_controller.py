7b3752c96f8a3700f3116938f6050c40f08b49cc
Daniel Shteinbok <dshteinbok@gmail.com>
2025-03-18 19:41:14 -0400

    added distance to status

--- raspberry_controller.py
+++ raspberry_controller.py
@@ -166,7 +166,7 @@ def read_serial():
                         # Save state
                         save_state(x.tolist(), P.tolist())
 
-                        status = f"volume: {x[0]}, flowrate: {x[1]}, tds: {tdsvalue}, temperature: {temperature}"
+                        status = f"volume: {x[0]}, flowrate: {x[1]}, tds: {tdsvalue}, temperature: {temperature}, ultrasonic distance: {distance}"
                         # print(status)
 
                         # print(f"[Arduino] {runtime}, {flow_counts}, {distance}")
